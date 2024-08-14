d2da5874ad01e10f2c8f8533999da011433c898b
shrawan-sk <shrawankhanal6@gmail.com>
2024-08-13 23:54:45 -0400

    Initial commit

---
+++ src/components/Main.jsx
@@ -0,0 +1,17 @@
+// Name: Shusan Pariyar 
+// College id: 301284265
+// tailwind css
+import React from 'react';
+import Nav from './Nav';
+import Home from './Home';
+
+const Main = () => {
+  return (
+    <>
+      <Nav />
+      <Home />
+    </>
+  );
+}
+
+export default Main;
